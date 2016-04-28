f6655af6816529f3217cda948bb3fbfd9f978aa8
paulbrowne <Paulmatthewbrowne@gmail.com>
2016-04-28 17:14:27 +0300

    Update matchHeights.js

--- matchHeights.js
+++ matchHeights.js
@@ -1,21 +1,56 @@
-(function(){
-    var matchHeight = document.querySelectorAll(".match-height");
-    var k = matchHeight.length;
-    while(k--){
-        var matchHeightChildren = matchHeight[k].children;
-        var i = matchHeightChildren.length;
-        var ele = matchHeightChildren[0].getBoundingClientRect();
-        var largest = ele.bottom - ele.top;
-        while(i--){
-            var el = matchHeightChildren[i].getBoundingClientRect();
-            var h = el.bottom - el.top;
-            if(h > largest) {
-                largest = h;
+// IIFE - Immediately-Invoked Function Expression
+(function() {
+    matchHeight = function() {
+
+        // container(s) of the elements you want to have matching heights
+        var container = document.querySelectorAll(".match-height");
+
+        // loop over all the containers
+        var k = container.length;
+        while (k--) {
+
+            // the elements within each container
+            var elements = container[k].children;
+
+            // reset height of first element for responsiveness
+            elements[0].style.height = "auto";
+
+            // use the first element as a starting point
+            var first = elements[0].getBoundingClientRect();
+            var largest = first.bottom - first.top;
+
+            // loop over all the elements within each container
+            var i = elements.length;
+            while (i--) {
+
+                // again reset the height for responsiveness
+                elements[i].style.height = "auto";
+
+                // calculate heights of all elements
+                var element = elements[i].getBoundingClientRect();
+                var height = element.bottom - element.top;
+                if (height > largest) {
+
+                    // store the height of the highest element within a container
+                    largest = height;
+                }
+            }
+
+            // loop back over the elements      
+            var j = elements.length;
+            while (j--) {
+
+                // apply the highest height to all elements within that container
+                elements[j].style.height = largest + "px";
             }
         }
-        var j = matchHeightChildren.length;
-        while(j--) {
-            matchHeightChildren[j].style.height = largest + "px";
-        }
-    }
+    };
+    window.addEventListener('resize', function() {
+
+        // run agian on resize
+        matchHeight();
+    });
 })();
+
+// init
+matchHeight();
